3d5ef74b25180b820a68b2e8884ec56f69cfbbe9
gurtodi <81238553+gurtodi@users.noreply.github.com>
2021-05-31 01:42:49 +0300

    Create 2_1.sh

---
+++ 2_1.sh
@@ -0,0 +1,11 @@
+gud@gud-VirtualBox:~$ pwd
+/home/gud
+gud@gud-VirtualBox:~$ cd /media/sf_Linux.WorkStation
+gud@gud-VirtualBox:/media/sf_Linux.WorkStation$ cd lesson2/
+gud@gud-VirtualBox:/media/sf_Linux.WorkStation/lesson2$ cd ../../
+gud@gud-VirtualBox:/media$ cd ~
+gud@gud-VirtualBox:~$ cd ..
+gud@gud-VirtualBox:/home$ cd
+gud@gud-VirtualBox:~$ cd Downloads
+gud@gud-VirtualBox:~/Downloads$ pwd
+/home/gud/Downloads
